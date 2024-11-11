96c14dd551e47c65fa55c59ef7492a4c9e36456d
gumeeee <gaxco123@gmail.com>
2024-11-11 11:55:51 -0300

    feat: User resources endpoint create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { TempModule } from './temp/temp.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsuarioModule, TempModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
