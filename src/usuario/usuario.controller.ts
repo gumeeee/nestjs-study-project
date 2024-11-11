646ab2b6329c0525f8ac186e7198a93797d367ce
gumeeee <gaxco123@gmail.com>
2024-11-11 15:09:39 -0300

    feat: Prisma setup released and create persistense data in prisma

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import Usuario from './usuario.entity';
+import { UsuarioRepository } from './usuario.repository';
 
 @Controller('usuario')
 export class UsuarioController {
+  constructor(private usuarioRepository: UsuarioRepository) {}
+
   @Post()
   async criar(@Body() usuario: Usuario) {
-    return { id: 100, ...usuario };
+    const novoUsuario = await this.usuarioRepository.create(usuario);
+    return novoUsuario;
   }
 }
